3a0e0cea7a1b57029e9d15c2543578f06094fe52
danijels <73341306+danijels@users.noreply.github.com>
2021-10-12 20:38:44 +0200

    Scripts and fixed project page link generating

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ import Footer from "../components/footer"
 import avatar from "../images/avatar.jpg"
 
 const { useState } = React;
-const host = typeof window !== 'undefined' && `${window.location.protocol}//${window.location.host}`;
 
 const IndexPage = ({ data: { about, categories } }) => {
     const allCategories = categories.nodes;
@@ -34,7 +33,7 @@ const IndexPage = ({ data: { about, categories } }) => {
 
     const pins = category.projects.map(({ title, shortDescription, pinPhoto, stack, queryParam, url }) => {
         const links = [
-            ["See More", `${host}/project?name=${queryParam}`],
+            ["See More", `/project?name=${queryParam}`],
             ["See Live", url],
             ["See Code", `https://github.com/danijels/${queryParam}`]
         ];
